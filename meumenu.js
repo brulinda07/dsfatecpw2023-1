df7616ee2b027e6248da32590f4c016974f705df
bruna <130687324+brunamel@users.noreply.github.com>
2023-06-22 22:25:44 -0300

    Add files via upload

---
+++ meumenu.js
@@ -0,0 +1,15 @@
+window.onload = function ()
+{
+}
+    var navbar = document.getElementById("navbar");
+    var sticky = navbar.offsetTop;
+    window.onscroll = function() {myFunction(navbar, sticky)};
+
+
+function myFunction(navbar, stickys){
+    if(window.pageYOffset >= sticky){
+        navbar.classList.add("sticky");
+    }else{
+        navbar.classList.remove("sticky");
+    }
+}
